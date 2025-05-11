fd48dea2efb2d07ddd453c55cfa9ecfb6b78b847
StarovoitKaterina <katrinblr1111@gmail.com>
2025-05-11 18:30:27 +0300

    Task 7_1

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -6,4 +6,50 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords() {}
+function capitalizeWords(str) {
+    let words = str.split(' ')
+    let newA = []
+    for (let i = 0; i < words.length; i++) {
+        let newWord = words[i].charAt(0).toUpperCase() + words[i].slice(1)
+        newA.push(newWord)
+
+    }
+    return newA.join(' ')
+}
+
+console.log(capitalizeWords('name new'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let name = 'kate new'
+let nwName = name.split(' ')
+console.log(nwName)
+console.log(nwName.join(' '))
+console.log(name.charAt(0).toUpperCase()+ name.slice(1))
+
+*/
+
+/*
+const string = 'Hello, World!'
+
+console.log(string.charAt(0)); // => H
+console.log(string.charAt(1)); // => e
+console.log(string.charAt(5)); // => ,
+
+// возвращаем последний символ строки
+console.log(string.charAt(string.length - 1)); // => !*/
