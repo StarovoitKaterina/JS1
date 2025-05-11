8707f1ee9459258bf15bcc4ef6d4cd229f19514f
StarovoitKaterina <katrinblr1111@gmail.com>
2025-05-11 19:01:12 +0300

    Task 7_2

--- lesson-07/02.js
+++ lesson-07/02.js
@@ -4,11 +4,17 @@
  */
 
 function isNumeric(str) {
-  // your code
+    let isNumber = !isNaN(Number(str))
+
+    if (str.trim() === '') {
+        return false
+    }
+    if (isNumber) {
+        return true
+    }
+    return false
+
 }
 
-// console.log(isNumeric("123")) // Ожидаемый результат: true
-// console.log(isNumeric("12.3")) // Ожидаемый результат: true
-// console.log(isNumeric("123abc")) // Ожидаемый результат: false
-// console.log(isNumeric("abc")) // Ожидаемый результат: false
-// console.log(isNumeric(" ")) // Ожидаемый результат: false
+console.log(isNumeric(" "))
+
