3bd6229cbf0c05969707c0691ef298c62136dc76
StarovoitKaterina <katrinblr1111@gmail.com>
2025-05-15 16:24:28 +0300

    Task 7_3

--- lesson-07/03.js
+++ lesson-07/03.js
@@ -10,5 +10,11 @@
 */
 
 function truncate(str, maxLength) {
-  // your code
+  if (str.length > maxLength) {
+      return str.slice(0,maxLength) + `${'...'}`
+  }
+  return str
 }
+
+console.log(truncate("Вот, что мне действительно нравится в этом", 20))
+
